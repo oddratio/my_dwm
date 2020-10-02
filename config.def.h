7cecd4e5f774cf24a6a3b704a05e416079264e8f
oddratio <emennesson@gmail.com>
2020-10-02 15:06:26 -0400

    updated font to use jet brains. Will probably have to fix if I pull to another computer

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,8 +32,8 @@ static const char s_base3[]         = "#fdf6e3";
 /*EM UPDATED COLOR SCHEME*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { s_base0, s_base03, s_base2 },      /* SchemeNorm dark */
-	[SchemeSel]  = { s_base0, s_base02, s_base2 },      /* SchemeSel dark */
+	[SchemeNorm] = { s_base0, s_base03, s_base01 },      /* SchemeNorm dark */
+	[SchemeSel]  = { s_base0, s_base02, s_base01 },      /* SchemeSel dark */
 };
 
 /* tagging */
