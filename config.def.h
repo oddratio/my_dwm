198a6cf913238cadf634b14c278e1b1b546ea681
emmanuel_m <emennesson@gmail.com>
2020-10-08 12:26:48 -0400

    modified branch for running on laptop with different terminal and different font name

--- config.def.h
+++ config.def.h
@@ -9,8 +9,11 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
+//static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10" };
+//static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
+//also on debain need sudo apt install libxft-dev
+static const char *fonts[]          = { "JetBrains Mono Variable:size=11" };
+static const char dmenufont[]       = "JetBrains Mono Variable:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -80,7 +83,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 /*eman's color changes*/
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", s_base03, "-nf", s_base0, "-sb", s_base2, "-sf", s_base0, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };//alacritty-ligatures-git
+static const char *termcmd[]  = { "kitty", NULL };//alacritty-ligatures-git
 static const char *firefoxcmd[]  = { "firefox", "-p" , NULL };
 
 static Key keys[] = {
